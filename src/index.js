1ea45eb19a8db67d4b06e22b77ecad3803a28888
ItsHope <80764464+ItsHope@users.noreply.github.com>
2023-06-24 22:42:25 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-   <App />
-   </BrowserRouter>
-  
-   
- 
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
+reportWebVitals();
 
